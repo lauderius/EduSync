a52e34f6bf26f8f19b77512579c5223924766fda
Marcos Laudério Eduardo Tavares <mt746728@gmail.com>
2025-12-07 07:15:37 +0100

    Delete 1.0 directory

--- 1.0/admin/login.php
+++
@@ -1,6 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";       // padrão no XAMPP
-$pass = "";           // senha vazia por default
-$db   = "edusync_db";
-?>
--- 1.0/test_connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-include 'db.php';
-echo "Conexão feita com sucesso!";
-?>
