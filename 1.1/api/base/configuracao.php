387299e2772d2034e1d208b6eeda0429983f4534
Marcos Laudério Eduardo Tavares <mt746728@gmail.com>
2025-12-07 07:15:56 +0100

    Delete 1.1 directory

--- 1.1/api/base/configuracao.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host'    => 'localhost',
-    'bd'      => 'edusync',   // nome da base de dados
-    'usuario' => 'root',      // user padrão do XAMPP
-    'senha'   => '',          // sem senha
-    'charset' => 'utf8mb4',
-];
